2fa0eafe740f72fde42d91236bff8d36e857f6fc
Chris Caragianis <chriscaragianis@gmail.com>
2017-04-26 16:02:58 -0400

    CSS logo, begin componentification

---
+++ modules/Stretch.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Measure from 'react-measure';
+
+class StretchBox extends Component {
+  state = {
+    width: -1
+  };
+
+  render() {
+    const
+  }
+};
+
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,40 @@
 import React from 'react';
+import Head from 'next/head';
+
 export default () => (
-  <div>Welcome to next.js!</div>
+  <div>
+    <Head>
+      <title>A title</title>
+      <link rel="stylesheet" href="/static/main.css"></link>
+    </Head>
+    <div>
+      <div className="louisville">
+        <img src="/static/logo.png"></img><br/>
+        <span className="cityname">
+          <span className="louis">
+            LOUIS<br/>
+          </span>
+          <span className="ville">
+            VILLE<br/>
+          </span>
+        </span>
+      </div>
+      <br/>
+      <div className="dsa">
+        DSA
+      </div>
+      <div className="words">
+        <span className="dem">
+          Democratic<br/>
+        </span>
+        <span className="soc">
+          Socialists<br/>
+        </span>
+        <span className="amer">
+          of America<br/>
+        </span>
+      </div>
+    </div>
+  </div>
+
 )
