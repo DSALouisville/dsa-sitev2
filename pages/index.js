6fdc0c357af61fb2cae5c3580e57737c7f16dd26
Chris Caragianis <chriscaragianis@gmail.com>
2017-04-28 22:05:45 -0400

    Add BlogPage

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import Head from 'next/head';
 import Logo from './Logo';
 import MenuBar from './MenuBar';
+import BlogPage from './BlogPage';
 export default () => (
   <div>
     <Head>
       <title>A title</title>
       <link rel="stylesheet" href="/static/main.css"></link>
     </Head>
-    <Logo/>
+    <Logo className="side"/>
     <MenuBar/>
+    <BlogPage/>
   </div>
 )
