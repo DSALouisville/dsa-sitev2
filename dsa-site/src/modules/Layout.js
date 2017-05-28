5e2c5ec9b9ca8848425a383f2d2524d71d4995ed
Chris Caragianis <chriscaragianis@gmail.com>
2017-05-28 09:24:55 -0400

    move files

--- dsa-site/src/modules/Layout.js
+++ dsa-site/src/modules/Layout.js
@@ -3,6 +3,8 @@ import Head from 'next/head'
 import { Container, Row, Col } from 'reactstrap'
 import Logo from '../modules/Logo';
 import MenuBar from '../modules/MenuBar';
+import 'main.css';
+import 'bootstrap.css';
 
 const Layout = (props) => (
   <div>
@@ -10,7 +12,6 @@ const Layout = (props) => (
       <title>Louisville DSA</title>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <link rel="stylesheet" href="/static/bootstrap..css" />
-      <link rel="stylesheet" href="/static/main.css" />
       <link rel="stylesheet" href="/static/rbc.css" />
     </Head>
     <Container>
